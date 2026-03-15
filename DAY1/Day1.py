f1154555fcaa2e39ecff59c2b392fd855c8acb8d
AsadH06 <138548586+AsadH06@users.noreply.github.com>
2026-03-15 10:00:00 +0400

    Day 01: Working with Variables

---
+++ DAY1/Day1.py
@@ -0,0 +1,14 @@
+print('hello world!')
+input("What is your name?")
+print("hello " + input("What is your name?")+"!")
+# download thonny for decoding
+name = "jack"
+print(name)
+name = "angela"
+print(name)
+#how to find length of a string -> len()
+print(len(name))
+print("Welcome to the Band Name generator.\n")
+city = input("What's the nme of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be "+city+" "+pet)
