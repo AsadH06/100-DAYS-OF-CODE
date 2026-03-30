d3c9a754fadb4a424f722931e2097bcaf313e057
AsadH06 <138548586+AsadH06@users.noreply.github.com>
2026-03-30 10:00:00 +0400

    Day 16: OOP — Classes & Objects

---
+++ DAY16/oop-coffee-machine-start/main.py
@@ -0,0 +1,35 @@
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+
+def coffee_machine():
+
+
+    while True:
+        choice = input(f"What would you like? ({menu.get_items()})").lower()
+
+
+
+        if choice == "off":
+            break
+        elif choice == "report":
+            coffee_maker.report()
+            money_machine.report()
+        else:
+            coffee = menu.find_drink(choice)
+            if coffee is None:
+                continue
+            if coffee_maker.is_resource_sufficient(coffee):
+                if money_machine.make_payment(coffee.cost):
+                    coffee_maker.make_coffee(coffee)
+
+
+
+
+coffee_machine()
+
